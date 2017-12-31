7c1fb944bec1ddd5d5d5a7130185b2431ac0ec89
ppvconadac <shagitee@gmail.com>
2017-12-31 16:42:10 +0800

    ADD BORROW

---
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCInventBCont.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BustosApartment_SAD_
+{
+    public partial class UCInventBCont : UserControl
+    {
+        public UCInventBCont()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
