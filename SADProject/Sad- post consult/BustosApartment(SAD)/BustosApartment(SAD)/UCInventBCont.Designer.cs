24e299764123bdd624bce405db37b30c377f380b
ppvconadac <shagitee@gmail.com>
2018-01-26 00:56:36 +0800

    ADD

--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCInventBCont.Designer.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCInventBCont.Designer.cs
@@ -79,6 +79,7 @@
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(398, 10);
             this.panel2.TabIndex = 40;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // label5
             // 
@@ -90,6 +91,7 @@
             this.label5.Size = new System.Drawing.Size(187, 25);
             this.label5.TabIndex = 39;
             this.label5.Text = "BORROWABLES";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // pictureBox1
             // 
@@ -102,6 +104,7 @@
             this.pictureBox1.Size = new System.Drawing.Size(28, 24);
             this.pictureBox1.TabIndex = 15;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // textBox5
             // 
@@ -110,6 +113,7 @@
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(183, 24);
             this.textBox5.TabIndex = 14;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
             // 
             // panel1
             // 
@@ -147,6 +151,7 @@
             this.dataGridView1.Size = new System.Drawing.Size(670, 557);
             this.dataGridView1.TabIndex = 43;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
             // 
             // tabControl1
             // 
@@ -159,6 +164,7 @@
             this.tabControl1.SelectedIndex = 0;
             this.tabControl1.Size = new System.Drawing.Size(403, 539);
             this.tabControl1.TabIndex = 41;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
             // 
             // tabPage2
             // 
@@ -191,6 +197,7 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(358, 23);
             this.textBox4.TabIndex = 66;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // textBox1
             // 
@@ -199,6 +206,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(358, 23);
             this.textBox1.TabIndex = 65;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
@@ -207,6 +215,7 @@
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(358, 23);
             this.textBox2.TabIndex = 63;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
@@ -215,6 +224,7 @@
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(358, 23);
             this.textBox3.TabIndex = 61;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label3
             // 
@@ -224,6 +234,7 @@
             this.label3.Size = new System.Drawing.Size(107, 18);
             this.label3.TabIndex = 64;
             this.label3.Text = "Damage Rate :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
@@ -233,6 +244,7 @@
             this.label4.Size = new System.Drawing.Size(119, 18);
             this.label4.TabIndex = 62;
             this.label4.Text = "Borrowing Rate :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label6
             // 
@@ -242,6 +254,7 @@
             this.label6.Size = new System.Drawing.Size(94, 18);
             this.label6.TabIndex = 60;
             this.label6.Text = "Actual Price :";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label18
             // 
@@ -252,6 +265,7 @@
             this.label18.Name = "label18";
             this.label18.Size = new System.Drawing.Size(0, 15);
             this.label18.TabIndex = 58;
+            this.label18.Click += new System.EventHandler(this.label18_Click);
             // 
             // button2
             // 
@@ -274,6 +288,7 @@
             this.txtin.Name = "txtin";
             this.txtin.Size = new System.Drawing.Size(358, 23);
             this.txtin.TabIndex = 44;
+            this.txtin.TextChanged += new System.EventHandler(this.txtin_TextChanged);
             // 
             // label14
             // 
@@ -283,6 +298,7 @@
             this.label14.Size = new System.Drawing.Size(80, 18);
             this.label14.TabIndex = 45;
             this.label14.Text = "Item Type :";
+            this.label14.Click += new System.EventHandler(this.label14_Click);
             // 
             // label15
             // 
@@ -292,6 +308,7 @@
             this.label15.Size = new System.Drawing.Size(88, 18);
             this.label15.TabIndex = 43;
             this.label15.Text = "Item Name :";
+            this.label15.Click += new System.EventHandler(this.label15_Click);
             // 
             // tabPage3
             // 
@@ -317,6 +334,7 @@
             this.tabPage3.Size = new System.Drawing.Size(395, 505);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Update Item";
+            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
             // 
             // button1
             // 
@@ -330,40 +348,45 @@
             this.button1.TabIndex = 75;
             this.button1.Text = "Archive";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // textBox6
             // 
             this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.textBox6.Location = new System.Drawing.Point(20, 178);
+            this.textBox6.Location = new System.Drawing.Point(20, 179);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(358, 23);
             this.textBox6.TabIndex = 74;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
             // 
             // textBox7
             // 
             this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.textBox7.Location = new System.Drawing.Point(20, 130);
+            this.textBox7.Location = new System.Drawing.Point(20, 131);
             this.textBox7.Name = "textBox7";
             this.textBox7.Size = new System.Drawing.Size(358, 23);
             this.textBox7.TabIndex = 72;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 154);
+            this.label1.Location = new System.Drawing.Point(16, 155);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(107, 18);
             this.label1.TabIndex = 73;
             this.label1.Text = "Damage Rate :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 106);
+            this.label2.Location = new System.Drawing.Point(16, 107);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(119, 18);
             this.label2.TabIndex = 71;
             this.label2.Text = "Borrowing Rate :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button3
             // 
@@ -382,26 +405,29 @@
             // txtuids
             // 
             this.txtuids.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.txtuids.Location = new System.Drawing.Point(20, 274);
+            this.txtuids.Location = new System.Drawing.Point(20, 275);
             this.txtuids.Name = "txtuids";
             this.txtuids.Size = new System.Drawing.Size(358, 23);
             this.txtuids.TabIndex = 69;
+            this.txtuids.TextChanged += new System.EventHandler(this.txtuids_TextChanged);
             // 
             // txtuit
             // 
             this.txtuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.txtuit.Location = new System.Drawing.Point(20, 80);
+            this.txtuit.Location = new System.Drawing.Point(20, 81);
             this.txtuit.Name = "txtuit";
             this.txtuit.Size = new System.Drawing.Size(358, 23);
             this.txtuit.TabIndex = 66;
+            this.txtuit.TextChanged += new System.EventHandler(this.txtuit_TextChanged);
             // 
             // txtuis
             // 
             this.txtuis.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.txtuis.Location = new System.Drawing.Point(20, 227);
+            this.txtuis.Location = new System.Drawing.Point(20, 228);
             this.txtuis.Name = "txtuis";
             this.txtuis.Size = new System.Drawing.Size(358, 23);
             this.txtuis.TabIndex = 64;
+            this.txtuis.TextChanged += new System.EventHandler(this.txtuis_TextChanged);
             // 
             // txtuin
             // 
@@ -410,15 +436,17 @@
             this.txtuin.Name = "txtuin";
             this.txtuin.Size = new System.Drawing.Size(358, 23);
             this.txtuin.TabIndex = 62;
+            this.txtuin.TextChanged += new System.EventHandler(this.txtuin_TextChanged);
             // 
             // label16
             // 
             this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(16, 253);
+            this.label16.Location = new System.Drawing.Point(16, 254);
             this.label16.Name = "label16";
             this.label16.Size = new System.Drawing.Size(150, 18);
             this.label16.TabIndex = 68;
             this.label16.Text = "Item Damage Status :";
+            this.label16.Click += new System.EventHandler(this.label16_Click);
             // 
             // label21
             // 
@@ -428,15 +456,17 @@
             this.label21.Size = new System.Drawing.Size(80, 18);
             this.label21.TabIndex = 65;
             this.label21.Text = "Item Type :";
+            this.label21.Click += new System.EventHandler(this.label21_Click);
             // 
             // label22
             // 
             this.label22.AutoSize = true;
-            this.label22.Location = new System.Drawing.Point(16, 203);
+            this.label22.Location = new System.Drawing.Point(16, 204);
             this.label22.Name = "label22";
             this.label22.Size = new System.Drawing.Size(90, 18);
             this.label22.TabIndex = 63;
             this.label22.Text = "Item Status :";
+            this.label22.Click += new System.EventHandler(this.label22_Click);
             // 
             // label23
             // 
@@ -446,6 +476,7 @@
             this.label23.Size = new System.Drawing.Size(88, 18);
             this.label23.TabIndex = 61;
             this.label23.Text = "Item Name :";
+            this.label23.Click += new System.EventHandler(this.label23_Click);
             // 
             // UCInventBCont
             // 
--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCInventBCont.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCInventBCont.cs
@@ -173,6 +173,161 @@ namespace BustosApartment_SAD_
             else {
             }
         }
+
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label18_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtuids_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtuit_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtuis_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtuin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label22_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label23_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
     
