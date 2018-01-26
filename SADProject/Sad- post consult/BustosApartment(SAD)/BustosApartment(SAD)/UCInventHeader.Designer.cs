6587c215eefe4247cbb1f5eae51b91fadeca9f8b
ppvconadac <shagitee@gmail.com>
2018-01-26 09:26:58 +0800

    z

--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCInventHeader.Designer.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCInventHeader.Designer.cs
@@ -91,7 +91,7 @@
             this.button5.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonShadow;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(13, 17);
+            this.button5.Location = new System.Drawing.Point(142, 17);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(123, 32);
             this.button5.TabIndex = 14;
@@ -148,7 +148,7 @@
             this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ButtonShadow;
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.Location = new System.Drawing.Point(142, 17);
+            this.button7.Location = new System.Drawing.Point(13, 17);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(123, 32);
             this.button7.TabIndex = 16;
