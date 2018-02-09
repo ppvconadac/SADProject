d84193e24509635a1cace5ef52f50300621250d9
0FruitCake <34014206+0FruitCake@users.noreply.github.com>
2018-02-09 09:04:40 +0800

    reserve_list

--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCRoomRContent.Designer.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCRoomRContent.Designer.cs
@@ -31,21 +31,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UCRoomRContent));
             this.panel2 = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtRoom = new System.Windows.Forms.TextBox();
-            this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.btnCreate = new System.Windows.Forms.Button();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
-            this.label2 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -68,26 +60,6 @@
             this.label5.TabIndex = 42;
             this.label5.Text = "RESERVATION";
             // 
-            // dataGridView1
-            // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(15, 45);
-            this.dataGridView1.MultiSelect = false;
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(333, 274);
-            this.dataGridView1.TabIndex = 45;
-            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
-            // 
             // dataGridView2
             // 
             this.dataGridView2.AllowUserToAddRows = false;
@@ -98,71 +70,14 @@
             this.dataGridView2.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dataGridView2.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(15, 325);
+            this.dataGridView2.Location = new System.Drawing.Point(15, 47);
             this.dataGridView2.MultiSelect = false;
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.ReadOnly = true;
             this.dataGridView2.RowHeadersVisible = false;
             this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView2.Size = new System.Drawing.Size(670, 274);
+            this.dataGridView2.Size = new System.Drawing.Size(670, 557);
             this.dataGridView2.TabIndex = 46;
-            // 
-            // txtName
-            // 
-            this.txtName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.txtName.Location = new System.Drawing.Point(712, 102);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(358, 23);
-            this.txtName.TabIndex = 48;
-            this.txtName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(709, 72);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(52, 18);
-            this.label1.TabIndex = 50;
-            this.label1.Text = "Name:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(709, 144);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(54, 18);
-            this.label3.TabIndex = 52;
-            this.label3.Text = "Room:";
-            // 
-            // txtRoom
-            // 
-            this.txtRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
-            this.txtRoom.Location = new System.Drawing.Point(712, 165);
-            this.txtRoom.Name = "txtRoom";
-            this.txtRoom.Size = new System.Drawing.Size(358, 23);
-            this.txtRoom.TabIndex = 51;
-            // 
-            // dataGridView3
-            // 
-            this.dataGridView3.AllowUserToAddRows = false;
-            this.dataGridView3.AllowUserToDeleteRows = false;
-            this.dataGridView3.AllowUserToResizeColumns = false;
-            this.dataGridView3.AllowUserToResizeRows = false;
-            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView3.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView3.BackgroundColor = System.Drawing.Color.White;
-            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(350, 45);
-            this.dataGridView3.MultiSelect = false;
-            this.dataGridView3.Name = "dataGridView3";
-            this.dataGridView3.ReadOnly = true;
-            this.dataGridView3.RowHeadersVisible = false;
-            this.dataGridView3.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView3.Size = new System.Drawing.Size(333, 274);
-            this.dataGridView3.TabIndex = 53;
-            this.dataGridView3.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView3_CellClick);
             // 
             // pictureBox1
             // 
@@ -190,56 +105,56 @@
             this.btnCreate.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonShadow;
             this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCreate.Location = new System.Drawing.Point(957, 268);
+            this.btnCreate.Location = new System.Drawing.Point(692, 100);
             this.btnCreate.Name = "btnCreate";
-            this.btnCreate.Size = new System.Drawing.Size(113, 32);
+            this.btnCreate.Size = new System.Drawing.Size(398, 55);
             this.btnCreate.TabIndex = 56;
             this.btnCreate.Text = "Reserve";
             this.btnCreate.UseVisualStyleBackColor = false;
             this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
             // 
-            // dateTimePicker1
+            // button1
             // 
-            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(712, 227);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(358, 20);
-            this.dateTimePicker1.TabIndex = 57;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(692, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(398, 55);
+            this.button1.TabIndex = 57;
+            this.button1.Text = "Reserve";
+            this.button1.UseVisualStyleBackColor = false;
             // 
-            // label2
+            // button2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(709, 206);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(102, 18);
-            this.label2.TabIndex = 58;
-            this.label2.Text = "Reserve Date:";
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(692, 289);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(398, 55);
+            this.button2.TabIndex = 58;
+            this.button2.Text = "Reserve";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // UCRoomRContent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnCreate);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.dataGridView3);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtRoom);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtName);
             this.Controls.Add(this.dataGridView2);
-            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.label5);
             this.Name = "UCRoomRContent";
             this.Size = new System.Drawing.Size(1104, 607);
             this.Load += new System.EventHandler(this.UCRoomRContent_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -249,17 +164,11 @@
         #endregion
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtRoom;
-        private System.Windows.Forms.DataGridView dataGridView3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button btnCreate;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCRoomRContent.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCRoomRContent.cs
@@ -32,8 +32,7 @@ namespace BustosApartment_SAD_
         public UCRoomRContent()
         {
             InitializeComponent();
-            tablecall();
-            tablecall2();
+           
             tablecall3();
         }
 
@@ -51,17 +50,7 @@ namespace BustosApartment_SAD_
         {
 
         }
-        public void tablecall() {
-            string quer = "select profile_name, profile_fname, profile_mname, profile_lname, user_id from profile";
-            dataGridView1.DataSource = c1.select(quer);
-            dataGridView1.Columns["User_ID"].Visible = false;
-        }
-        public void tablecall2()
-        {
-            string quer = "select room_number, room_id from room where room_status  = 'Available'";
-            dataGridView3.DataSource = c1.select(quer);
-            dataGridView3.Columns["room_id"].Visible = false;
-        }
+       
         public void tablecall3()
         {
             string quer = "select room_number, profile_name, CONCAT(profile_fname, profile_mname, profile_lname) as Name" +
@@ -69,47 +58,15 @@ namespace BustosApartment_SAD_
             dataGridView2.DataSource = c1.select(quer);
         }
 
-        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex > -1)
-            {
-                txtName.Text = dataGridView1.Rows[e.RowIndex].Cells["profile_name"].Value.ToString();
-                id = int.Parse(dataGridView1.Rows[e.RowIndex].Cells["user_id"].Value.ToString());
-            }
-        }
+     
 
-        private void dataGridView3_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex > -1)
-            {
-                txtRoom.Text = dataGridView3.Rows[e.RowIndex].Cells["room_number"].Value.ToString();
-                id1 = int.Parse(dataGridView3.Rows[e.RowIndex].Cells["room_id"].Value.ToString());
-            }
-        }
+      
 
         private void btnCreate_Click(object sender, EventArgs e)
         {
-            DateTime check;
-            if (txtName.Text != "" && txtRoom.Text != "") {
-                if (DateTime.TryParse(dateTimePicker1.Text, out check) && check < DateTime.Now)
-                {
-                    MessageBox.Show("Date has passed!", "Input error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                  
-                }
-                else {
-                    string quer = "insert into reservation values(NULL, '" + dateTimePicker1.Text + "', " + id + ", " + id1 + "  )";
-                    c1.insert(quer);
-                    string quer2 = "update room set room_status = 'Reserved' where room_id = "+id1+"" ;
-                    c1.insert(quer2);
-                    MessageBox.Show("Data Has Been Updated!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    txtName.Text = "";
-                    txtRoom.Text = "";
-                    tablecall();
-                    tablecall2();
-                    tablecall3();
+          
 
-                }
-            }
+              
 
         }
 
