32b073e6d701d9c7a1d8b1aa5ae7f977aca817ad
0FruitCake <34014206+0FruitCake@users.noreply.github.com>
2018-03-17 16:18:54 +0800

    reports

--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCIncomeContent.Designer.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCIncomeContent.Designer.cs
@@ -80,8 +80,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(398, 36);
             this.button1.TabIndex = 47;
-            this.button1.Text = "PRINT REPORT";
+            this.button1.Text = "Create Report";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // UCIncomeContent
             // 
--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCIncomeContent.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCIncomeContent.cs
@@ -7,12 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+
 
 namespace BustosApartment_SAD_
 {
     public partial class UCIncomeContent : UserControl
     {
         private static UCIncomeContent _instance;
+        Class1 c = new Class1();
 
         public static UCIncomeContent Instance
         {
@@ -26,10 +32,86 @@ namespace BustosApartment_SAD_
         public UCIncomeContent()
         {
             InitializeComponent();
+            tablecall();
         }
 
         private void UCIncomeContent_Load(object sender, EventArgs e)
         {
+
+        }
+        public void tablecall() {
+
+            string quer = "select profile_name , rt_date_start ,room_number,rc_rate,rt_type from room_transaction inner join room " +
+                "inner join profile inner join room_classification where room_transaction.Profile_user_ID = user_ID and room_transaction.Room_Room_ID = Room_ID" +
+                " and Room_classification_classification_ID = classification_ID and rt_date_start like '2018-2-%' and rt_type != 'Archived'";
+            dataGridView1.DataSource = c.select(quer);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            Document doc = new Document(iTextSharp.text.PageSize.LETTER, 10, 10, 42, 35);
+                PdfWriter wri = PdfWriter.GetInstance(doc, new FileStream("C:\\Users\\USER\\Documents\\GitHub\\SADProject\\text.pdf", FileMode.Create));
+                doc.Open();
+                Paragraph par = new Paragraph("BUSTOS APARTMENT MONTHLY INCOME\n\n");
+                par.Alignment = Element.ALIGN_CENTER;
+                doc.Add(par);
+            
+                string quer = "select room_number, room_id from room order by room_number";
+                DataTable d = c.select(quer);
+         
+
+            for (int  i = 0; i <d.Rows.Count; i++) {
+                Paragraph l = new Paragraph();
+                iTextSharp.text.Font font = new iTextSharp.text.Font(FontFactory.GetFont("Arial", 20, iTextSharp.text.Font.BOLD));
+                iTextSharp.text.Font font2 = new iTextSharp.text.Font(FontFactory.GetFont("Arial", 13, iTextSharp.text.Font.NORMAL));
+                l.IndentationLeft = 30f;
+                l.Add(new Chunk("Room " + d.Rows[i][0],font));
+                l.Add(new Paragraph("______________________________________________________________________\n"));
+                   doc.Add(l);
+                string quer2 = "select room_number, rt_date_start, profile_name, rt_type, rt_price, rt_extend_start from room inner join room_transaction" +
+            " inner join profile where room_id = Room_Room_ID and Profile_user_ID = user_ID and room_number = "+d.Rows[i][0]+" and rt_type != 'Archived' and rt_date_start like '2018-2-%'";
+                DataTable d2 = c.select(quer2);
+                string quer3 = "SELECT sum(rt_price) FROM ba_db.room_transaction where Room_Room_ID = "+ d.Rows[i][1] + " and rt_type != 'Archived' and rt_date_start like '2018-2-%'";
+                DataTable d3 = c.select(quer3);
+                if (d2.Rows.Count == 0) {
+
+                    Paragraph l2 = new Paragraph();
+                    l2.IndentationLeft = 40f;
+                    l2.Add(new Chunk("No Transacions This Month\n\n", font2));
+                    doc.Add(l2);
+                }
+                for (int j = 0; j < d2.Rows.Count; j++) {
+                    string x;
+                    if (d2.Rows[j][5].ToString() == "")
+                    {
+                        x = "Check-In";
+                    }
+                    else {
+                        x = "Extension";
+                    }
+                        Paragraph l3 = new Paragraph();
+                        l3.IndentationLeft = 40f;
+                        l3.Add(new Chunk("Name: " + d2.Rows[j][2] + "\n", font2));
+                        l3.Add(new Chunk("Date: " + d2.Rows[j][1] + "\n", font2));
+                        l3.Add(new Chunk("Amount: " + d2.Rows[j][4] + "\n", font2));
+                        l3.Add(new Chunk("Type: " + x + "\n\n", font2));
+                        doc.Add(l3);
+                
+                }
+                if (d3.Rows[0][0].ToString() != "")
+                {
+                    Paragraph l4 = new Paragraph();
+                    l4.IndentationLeft = 40f;
+                    l4.Add(new Chunk("TOTAL AMOUNT : " + d3.Rows[0][0] + "\n\n", font2));
+                    doc.Add(l4);
+                }
+
+            }
+            
+            MessageBox.Show("PDF Created, OK!");
+            doc.Close();
 
         }
     }
