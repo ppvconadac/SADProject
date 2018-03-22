c1c30821d72b9cf7ad6d32871c10b951a66639c9
ppvconadac <shagitee@gmail.com>
2018-03-22 23:52:14 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/ppvconadac/SADProject""
    This reverts commit 96328156b2de016b516d9111a9f14a03753543c9.

--- SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCRoomAsContent.Designer.cs
+++ SADProject/Sad- post consult/BustosApartment(SAD)/BustosApartment(SAD)/UCRoomAsContent.Designer.cs
@@ -38,6 +38,7 @@
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -189,10 +190,25 @@
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.Control;
+            this.button6.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(692, 507);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(398, 53);
+            this.button6.TabIndex = 64;
+            this.button6.Text = "Verify Payment";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // UCRoomAsContent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.dataGridView2);
@@ -225,5 +241,6 @@
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
     }
 }
